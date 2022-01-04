56c050bc6b3ea02eeb8598738610ec54742efdcb
Jose R Hilarión <hilarionjose@yahoo.es>
2022-01-04 09:23:01 -0500

    Primer commit. Luego del primer run.

---
+++ src/main/java/com/josehilarion/cartalimite/CartalimiteApplication.java
@@ -0,0 +1,13 @@
+package com.josehilarion.cartalimite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartalimiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartalimiteApplication.class, args);
+	}
+
+}
